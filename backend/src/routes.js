a02bb800341d0f9ef464fec43b1800741d6e84cd
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2019-10-15 14:20:13 -0300

    creating and listing spots

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+
 const SessionController = require("./controllers/SessionController");
+const SpotController = require("./controllers/SpotController");
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
+/**
+ * User routes
+ */
 routes.post("/sessions", SessionController.store);
+
+/**
+ * Spot routes
+ */
+routes.get("/spots", SpotController.index);
+routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
 module.exports = routes;
