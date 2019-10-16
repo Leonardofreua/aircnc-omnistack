2b44d5bed00f4c8556bd7296d445900b578d34e9
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2019-10-16 12:21:25 -0300

    added cors and connect with session route from api

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const routes = require("./routes");
 
 const app = express();
@@ -17,6 +19,7 @@ mongoose.connect(
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar body da requisição (para criação)
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
