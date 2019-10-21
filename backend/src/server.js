07b6e7b747c30dc3f434ea853e0ddf1c2ed187be
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2019-10-21 13:44:29 -0300

    The images are now being displayed.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const routes = require("./routes");
 
@@ -19,8 +20,9 @@ mongoose.connect(
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar body da requisição (para criação)
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
